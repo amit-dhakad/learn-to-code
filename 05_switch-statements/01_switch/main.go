98596b8485174c3a91a1ee02ef787078d8ec1998
Amit Dhakad <Amit.Dhakad@katerra.com>
2020-04-18 14:46:28 +0530

    variables, switch, for, if else

---
+++ 05_switch-statements/01_switch/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	switch "Medhi" {
+
+	case "Daniel":
+		fmt.Println("Wassup Daniel")
+	case "Medhi":
+		fmt.Println("Wassup Medhi") //
+	case "Jenny":
+		fmt.Println("Wassup Jenny")
+	default: //
+		fmt.Println("Have you no friends?")
+	}
+
+	/*
+			no default fallthrough
+			fallthrough is optional
+
+	*/
+
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+const p string = "amit dhakad"
+const pa = "amit const"
+
+const paa = "dhakad const"
+
+func main() {
+
+	const a string = "in function"
+	const ap = "inin function"
+	const aapp = "function n"
+
+	fmt.Println("p - ", p)
+	fmt.Println("pa - ", pa)
+	fmt.Println("paa - ", paa)
+	fmt.Println("a - ", a)
+	fmt.Println("ap - ", ap)
+	fmt.Println("aapp - ", aapp)
+
+}
